cd00ed79db929bc2f9b5d135b099db8d5f553267
Ahmad Abbas <73943807+ahmad-m-abbas@users.noreply.github.com>
2024-06-05 15:55:35 +0300

    Update options.py

--- reconstruction/options.py
+++ reconstruction/options.py
@@ -20,8 +20,8 @@ class Options:
 
         self.data_name = {'rsna': 'RSNA', 'vin': 'VinDr-CXR', 'brain': 'Brain Tumor', 'lag': 'LAG', 'isic': 'ISIC2018',
                           'c16': 'Camelyon16', 'brats': 'BraTS2021'}
-        self.epochs = {'rsna': 250, 'vin': 250, 'brain': 600, 'lag': 250, 'brats': 250,
-                       'oct': 250, 'colon': 250}
+        self.epochs = {'rsna': 125, 'vin': 125, 'brain': 600, 'lag': 125, 'brats': 125,
+                       'oct': 125, 'colon': 125}
         self.in_c = {'c16': 3, 'colon': 1}
 
     def parse(self):
